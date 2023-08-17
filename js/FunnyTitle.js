7bfb5c1e1e90decc6d65cea84fa4007c993b9cd4
tangfengtao <tft15862283365@gmail.com>
2023-08-17 22:07:33 +0800

    Site updated: 2023-08-17 22:07:32

---
+++ js/FunnyTitle.js
@@ -0,0 +1,16 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', '/funny.ico');
+        document.title = '😢页面崩溃啦 ~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', '/favicon.ico');
+        document.title = '🤣噫又好啦 ~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
